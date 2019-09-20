45fca24a9072f10d3eca4d2a3953f3150409efcc
Peter Powell <petpow@saberuk.com>
2019-09-20 10:48:59 +0100

    Commit docs updates as InspIRCd Robot not Travis CI.

--- tools/travis-ci.sh
+++ tools/travis-ci.sh
@@ -5,6 +5,8 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 	sudo apt-get install python3-pip --assume-yes --install-recommends
 	pip3 install markdown-include mkdocs mkdocs-cinder mkdocs-exclude
+	git config --global user.name "InspIRCd Robot"
+	git config --global user.email "noreply@inspircd.org"
 	mkdocs gh-deploy --force --message 'Update website for docs commit {sha}.' --remote-branch gh-pages --remote-name https://${GITHUB_TOKEN}@github.com/inspircd/inspircd-docs.git
 else
 	sudo apt-get install ruby --assume-yes --install-recommends
