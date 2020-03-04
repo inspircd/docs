a6428ba0272d6e7e097e6aad57335349bd94a940
Sadie Powell <sadie@witchery.services>
2020-03-04 21:16:13 +0000

    Switch Travis to use a pip requirements file.

--- tools/travis-ci.sh
+++ tools/travis-ci.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]
 then
 	sudo apt-get install python3-pip --assume-yes --install-recommends
-	pip3 install markdown-include mkdocs==1.0.4 mkdocs-cinder mkdocs-exclude mkdocs-redirects
+	pip3 install -r "${TRAVIS_BUILD_DIR}/requirements.txt"
 	git config --global user.name "InspIRCd Robot"
 	git config --global user.email "noreply@inspircd.org"
 	mkdocs gh-deploy --force --message 'Update website for docs commit {sha}.' --remote-branch gh-pages --remote-name https://${GITHUB_TOKEN}@github.com/inspircd/inspircd-docs.git
