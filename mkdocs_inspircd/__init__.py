inspircd/docs

commit d4e06e93119515f4c538a5908da3b40b27ddeedc

Dynamically fill version in page titles (#162).
This will allow using the same files between v3 and v4 docs without copying
the whole file (just a symlink)

mkdocs_inspircd/__init__.py
@@ -189,7 +189,9 @@ class InspircdPlugin(mkdocs.plugins.BasePlugin):
             return segments[0]
 
     def on_page_markdown(self, markdown, page, config, files):
-        """Runs Jinja on an input markdown file, to produce another markdown file."""
+        """Runs Jinja on an input markdown file, to produce another markdown file.
+        Also renders metadata at the beginning of the file as a side-effect."""
+
         env = jinja2.Environment(
             loader=jinja2.FileSystemLoader([TEMPLATES_DIR, config["docs_dir"]]),
         )
@@ -206,5 +208,13 @@ class InspircdPlugin(mkdocs.plugins.BasePlugin):
             "server_messages": self.server_messages(config),
             "version": version,
         }
+
+        # Render page metadata, by mutating arguments.
+        page.meta = {
+            k: env.from_string(v).render(context)
+            for (k, v) in page.meta.items()
+            if isinstance(v, str)
+        }
+
         template = env.from_string(markdown)
         return template.render(context)

commit e537a460a2a0753f37eb104c53e05be1ed78add4

Don't drop non-string metadata values
Broken by d4e06e93119515f4c538a5908da3b40b27ddeedc

mkdocs_inspircd/__init__.py
@@ -211,9 +211,8 @@ class InspircdPlugin(mkdocs.plugins.BasePlugin):
 
         # Render page metadata, by mutating arguments.
         page.meta = {
-            k: env.from_string(v).render(context)
+            k: env.from_string(v).render(context) if isinstance(v, str) else v
             for (k, v) in page.meta.items()
-            if isinstance(v, str)
         }
 
         template = env.from_string(markdown)